1aa5b6a2368813dd61d71a1e6fa15c00884538c6
GeroA6 <aguerogeronimo954@gmail.com>
2026-05-06 15:48:03 -0300

    Update: App.java
    - Se instancia la tarea y se le asigna a los hilos.
    - Antes de ejecutarlos se les asigna la prioridad. Esta prioridad es una sugerencia para la CPU, que se puede interpretar que en un momento de sobrecarga tenga preferencia al hilo con mayor prioridad. Todos los hilos por defecto tienen una prioridad de 5.

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        TareaRunnable tarea = new TareaRunnable();
+
+        Thread hilo1 = new Thread(tarea, "Hilo-Baja-Prioridad");
+        Thread hilo2 = new Thread(tarea, "Hilo-Alta-Prioridad");
+
+        hilo1.setPriority(1);  // Prioridad mínima
+        hilo2.setPriority(10); // Prioridad máxima
+
+        hilo1.start();
+        hilo2.start();
     }
 }
