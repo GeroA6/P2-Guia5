60406c4203fe7c5b34c1e6118cbc207b4b4d5dc1
GeroA6 <aguerogeronimo954@gmail.com>
2026-05-06 15:06:47 -0300

    Update: App.java
    - Se instancia una nueva tarea y se la pasa por parámetros a los nuevos objetos "Thread"

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Creamos una única instancia de la tarea
+        TareaRunnable tarea = new TareaRunnable();
+        
+        // Le pasamos la tarea al constructor de la clase Thread (y opcionalmente le damos un nombre al hilo)
+        Thread hilo1 = new Thread(tarea, "Hilo 1");
+        Thread hilo2 = new Thread(tarea, "Hilo 2");
+        
+        hilo1.start();
+        hilo2.start();
     }
 }
