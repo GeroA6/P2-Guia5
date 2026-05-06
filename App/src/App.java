70042fb064f4755c09d00e7755693e3462518c06
GeroA6 <aguerogeronimo954@gmail.com>
2026-05-06 16:23:41 -0300

    Update: App.java
    - Se instancia el hilo secundario y a mitad de su ejecución se interrumpe con el método "interrupt()", esto lanza la excepción y produce que el el hilo secundario salga de su bucle y se ejecute el bloque "cach" terminando su ejecución, el hilo principal "main" que estaba esperando por el método "join()" detecta que finalizo y termina el programa.

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        TareaLargaDuracion tarea = new TareaLargaDuracion();
+        Thread hiloSecundario = new Thread(tarea, "Hilo-Secundario");
+
+        hiloSecundario.start();
+
+        // Pausamos el hilo principal por 3 segundos para que el hilo secundario avance un poco
+        Thread.sleep(3000);
+
+        // En lugar de esperar a que termine todos los pasos, lo interrumpimos
+        System.out.println("Hilo Principal: Interrumpiendo el hilo secundario...");
+        hiloSecundario.interrupt();
+
+        // Utilizamos join() para esperar a que el hilo secundario termine de cerrar tras la interrupción
+        hiloSecundario.join();
+        System.out.println("Hilo Principal: Hilo secundario finalizado. Fin del programa.");
     }
 }
